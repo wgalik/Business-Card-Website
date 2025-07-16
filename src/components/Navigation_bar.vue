44ed7b4b2274aad597ab9ce7e164fdc83d005982
Wojtek Galik <w.galik@poczta.fm>
2025-07-16 21:35:29 +0200

    NavBar commit

--- src/components/Navigation_bar.vue
+++ src/components/Navigation_bar.vue
@@ -15,16 +15,20 @@
       </button>
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-          <li v-show="isEN" v-for="page in pagesEN" :key="page.id" class="nav-item">
-            <a class="nav-link" :href="page.href" :id="page.id" @click="navigate">{{
-              page.name
-            }}</a>
-          </li>
-          <li v-show="!isEN" v-for="page in pagesPL" :key="page.id" class="nav-item">
-            <a class="nav-link" :href="page.href" :id="page.id" @click="navigate">{{
-              page.name
-            }}</a>
-          </li>
+          <ul class="navbar-nav ms-auto mb-2 mb-lg-0" v-if="isEN">
+            <li v-for="page in pagesEN" :key="page.id" class="nav-item">
+              <a class="nav-link" :href="page.href" :id="page.id" @click="navigate">{{
+                page.name
+              }}</a>
+            </li>
+          </ul>
+          <ul class="navbar-nav ms-auto mb-2 mb-lg-0" v-if="!isEN">
+            <li v-for="page in pagesPL" :key="page.id" class="nav-item">
+              <a class="nav-link" :href="page.href" :id="page.id" @click="navigate">{{
+                page.name
+              }}</a>
+            </li>
+          </ul>
           <li class="nav-item">
             <button :id="isEN ? 'langBtnPL' : 'langBtnEN'" @click="toggleLang"></button>
           </li>
@@ -59,6 +63,8 @@ const pagesPL = [
 ]
 
 const toggleLang = () => {
+  console.log(isEN.value)
+
   isEN.value = !isEN.value
   if (isEN.value === true) {
     document.documentElement.lang = 'en'
