2f9f8eaff13621ab2b2c3801ef0c3cf7ef238361
Wojtek Galik <w.galik@poczta.fm>
2025-08-07 10:49:00 +0200

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,23 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools()],
+  plugins: [
+    vue(),
+    vueDevTools(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'dist/index.html',
+          dest: '.',
+          rename: '404.html',
+        },
+      ],
+    }),
+  ],
   base: '/Business-Card-Website/',
   resolve: {
     alias: {
